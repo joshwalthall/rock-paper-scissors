10f5a4b76d89e6b01ecaddf4fbf372ae0a77c9e8
Josh Walthall <josh.walthall@gmail.com>
2023-05-06 10:55:50 -0400

    Create getComputerChoice function that returns random selection from validChoices list

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Initialize variable to hold valid choices
-let validChoices = ['rock', 'paper', 'scissors']
+const validChoices = ['rock', 'paper', 'scissors'];
 // Get computer choice
+function getComputerChoice() {
+    // Create variable to store computer choice
+    let computerSelection = '';
+    // Generate a random number between 0 and 2
+    let randomIndex = Math.floor(Math.random() * 3);
+    // Use number to assign choice to computer choice variable
+    computerSelection = validChoices[randomIndex];
+    // Return that value
+    return computerSelection;
+}
+
 // Get player choice
 // Compare choices to determine winner
 // Output final verdict message
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
