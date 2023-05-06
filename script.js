e39bf051cff04ffac77e9970917320c88f6fb352
Josh Walthall <josh.walthall@gmail.com>
2023-05-06 10:49:10 -0400

    Create variable to store valid choices

---
+++ script.js
@@ -0,0 +1,6 @@
+// Initialize variable to hold valid choices
+let validChoices = ['rock', 'paper', 'scissors']
+// Get computer choice
+// Get player choice
+// Compare choices to determine winner
+// Output final verdict message
